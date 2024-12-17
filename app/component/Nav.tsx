50de8fe911f3527826bad8b5a81f1da927f7921c
owlushorler <owlushorler@gail.com>
2024-12-17 16:43:44 +0100

    homr

--- app/component/Nav.tsx
+++ app/component/Nav.tsx
@@ -1,21 +1,64 @@
+"use client"
+import { useState } from "react";
 
 
 const Navigation = () => {  
+const [update , setUpdate]=useState(false)
+
   return (  
-    <nav className="bg-white p-4 flex px-20 justify-between space-x-8"> {/* White background, padding, horizontal alignment, spacing */}  
+    <div className=" flex flex-col">
+    <nav className="bg-white p-4  flex flex-row px-20 justify-between space-x-8"> {/* White background, padding, horizontal alignment, spacing */}  
     <div>
-        <a className="text-blue-900 hover:underline">logo</a>  
+        <a className="text-blue-900 hover:underl
+        ine">logo</a>  
     </div>
-    <div className='flex gap-6' >
-        <a className="text-blue-900 hover:underline">Our Subsidiaries</a>  
-    
-        <a className="text-blue-900 hover:underline">About Us</a>  
+    <div onClick={()=>setUpdate((ele)=>ele?false:true)}  className="md:hidden text-2xl cursor-pointer" >
+    &equiv;
+    </div>
+
      
-        <a className="text-blue-900 hover:underline">Contact Us</a>  
+
+        <div className=' hidden md:flex md:flex-row gap-6' >
+        <a className="text-blue-900 hover:underline cursor-pointer">Our Subsidiaries</a>  
     
-        <a className="text-blue-900 hover:underline">Career</a>  
+        <a className="text-blue-900 cursor-pointer hover:underline">About Us</a>  
+     
+        <a className="text-blue-900 cursor-pointer hover:underline">Contact Us</a>  
+    
+        <a className="text-blue-900 cursor-pointer hover:underline">Career</a> 
+
+      
+        
         </div>
+
+        
+
+
+
+        
     </nav>  
+    { update && <div className='md:hidden flex flex-col gap-6' >
+        <a className="text-blue-900 cursor-pointer hover:underline">Our Subsidiaries</a>  
+    
+        <a className="text-blue-900 cursor-pointer hover:underline">About Us</a>  
+     
+        <a className="text-blue-900 cursor-pointer hover:underline">Contact Us</a>  
+    
+        <a className="text-blue-900 cursor-pointer hover:underline">Career</a>  
+        </div>}
+
+
+
+    
+
+    </div>
+
+
+
+
+
+
+
   );  
 };  
 
